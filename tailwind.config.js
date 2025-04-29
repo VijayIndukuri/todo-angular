7288c94631e97889e9b7e020c25548157a521263
VijayIndukuri <75474266+VijayIndukuri@users.noreply.github.com>
2025-04-29 18:03:23 +0200

    downgrading tailwindcss to v3

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#6e38bd',
+      },
+    },
+  },
+  plugins: [],
+} 
